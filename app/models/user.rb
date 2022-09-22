b4e8729ead3a5eb8ec392d2e12f72587c31b33d8
Yami Pedroso-AI <yamirovinci@gmail.com>
2022-09-22 11:58:26 +0200

    Almost finishing authtentication UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :offers
   has_many :appointments
+  validates :name, presence: true
   validates :email, :password, presence: true
   validates :email, uniqueness: true
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_151707) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_071756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,6 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_151707) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
